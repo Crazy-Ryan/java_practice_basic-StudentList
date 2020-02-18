5bcf792210cacd76c2220ddf20aed28b5757ab71
Ryan.Wang <ranwang1995@outlook.com>
2020-02-19 01:22:59 +0800

    [WangRan] feat: override toString method

--- src/main/java/com/thoughtworks/Student.java
+++ src/main/java/com/thoughtworks/Student.java
@@ -3,7 +3,7 @@ package com.thoughtworks;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Objects;
+
 
 public class Student {
 
@@ -14,7 +14,7 @@ public class Student {
     public Student(String name, String number, String admissionDateStr) throws ParseException {
         this.name = name;
         this.number = number;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.mm.dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
         this.admissionDate = simpleDateFormat.parse(admissionDateStr);
     }
 
@@ -23,9 +23,21 @@ public class Student {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return this.number.equals(((Student) o).number);
+        return this.number.equals(student.number);
     }
 
+    @Override
+    public String toString() {
+        Date now = new Date();
+        long yearOfStudy = Math.floorDiv((now.getTime() - admissionDate.getTime()), 31536000000L);
+        SimpleDateFormat newSDF = new SimpleDateFormat("yyyy年MM月dd日");
+        String admissionDay = newSDF.format(admissionDate);
+        return "我叫" + name
+                + ", 我的学号是" + number
+                + ", " + admissionDay + "入学"
+                + ", 学龄" + yearOfStudy + "年";
+
+    }
 
     public String getName() {
         return name;
