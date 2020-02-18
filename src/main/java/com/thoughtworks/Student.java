31d54b78f84ef89d57a6589767db55c81f8d7ea9
Ryan.Wang <ranwang1995@outlook.com>
2020-02-19 00:44:25 +0800

    [WangRan] feat: override equals method

--- src/main/java/com/thoughtworks/Student.java
+++ src/main/java/com/thoughtworks/Student.java
@@ -3,6 +3,7 @@ package com.thoughtworks;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class Student {
 
@@ -13,9 +14,18 @@ public class Student {
     public Student(String name, String number, String admissionDateStr) throws ParseException {
         this.name = name;
         this.number = number;
-        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("yyyy.mm.dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.mm.dd");
         this.admissionDate = simpleDateFormat.parse(admissionDateStr);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return this.number.equals(((Student) o).number);
+    }
+
 
     public String getName() {
         return name;
