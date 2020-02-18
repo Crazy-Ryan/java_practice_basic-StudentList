a74f330a9958353120d3d8b8c7da425cdb8c40b0
Ryan.Wang <ranwang1995@outlook.com>
2020-02-19 02:26:57 +0800

    [WangRan] feat: finish main method in class App

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,7 +1,15 @@
 package com.thoughtworks;
 
+import java.text.ParseException;
+
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ParseException {
+
+    Teacher zhangLong = new Teacher("张龙");
+    zhangLong.addNewStudent(new Student("张三", "001", "2019.02.11"));
+    zhangLong.addNewStudent(new Student("李四", "002", "2019.02.10"));
+    zhangLong.addNewStudent(new Student("王五", "001", "2019.02.09"));
+    zhangLong.findDuplicateStudent();
   }
 }
